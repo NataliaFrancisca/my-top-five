f15dd40d78956b40f23795f8f6f6917c9e120ae3
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-21 20:34:11 -0300

    feat: added a background for the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className='bg-white'>
+    <html lang="en" className='bg-c_white'>
       <body className={worksans.className}>{children}</body>
     </html>
   );
