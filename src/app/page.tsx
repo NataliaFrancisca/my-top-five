e66dc61fbd2e7f8aba67dfa96a942cd71799befe
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-21 20:34:35 -0300

    feat: created the initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+'use client';
+import Image from "next/image";
+import Button from "./components/Button/Button";
+
 export default function Home() {
   return (
-    <main>
-      <h1>testeee</h1>
+    <main className="h-dvh flex justify-center items-center gap-2.5 p-6" >
+      
+      <Image 
+        src="title.svg" 
+        width={84} 
+        height={600}
+        className="h-full flex"
+        alt="image that have a duplicated phrase my top five written in green with a black background"
+      />
+
+      <section className="flex flex-col h-fit">
+        <Image
+          src="illustration.svg"
+          width={258}
+          height={481}
+          className="h-full flex"
+          alt="image that have a duplicated person listening music using headphones"
+        />
+
+        <Button message="TRY WITH SPOTIFY" style="w-11/12" buttonEvent={() => console.log("BUTTON WAS CLICKED")}/>
+      </section>
     </main>
   );
 }
