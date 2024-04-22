9c46ef0f276f01f768e1b1921bfe9ab15ba16aeb
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-22 19:57:21 -0300

    refactor: removed the class self-center

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 export const Button = (props: {message: string, style: string, buttonEvent: () => void}) => {
 
-    const default_style = "mt-6 py-5 border-solid border-[3px] rounded-sm border-c_black font-black text-c_black self-center transition ease-in-out delay-150 duration-300 hover:bg-primary"
+    const default_style = "mt-6 py-5 border-solid border-[3px] rounded-sm border-c_black font-black text-c_black transition ease-in-out delay-150 duration-300 hover:bg-primary"
 
     return (
         <button className={`${default_style} ${props.style}`} onClick={() => props.buttonEvent()}>
