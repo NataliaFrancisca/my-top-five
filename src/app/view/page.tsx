0cb40a4f3a70ff168506610a4065a04097cb8c34
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-22 19:38:01 -0300

    feat: import component viewer and button

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+import Button from "../components/Button/Button";
+import Viewer from "../components/Viewer/Viewer";
+
 const Page = () => {
     return(
-        <main>
-            <h1>VIEW PAGE</h1>
+        <main className="py-10 px-6 flex flex-col md:w-6/12 md:mx-auto lg:w-4/12">
+            <Viewer />
+            <Button message="DOWNLOAD" style="w-8/12 self-end" buttonEvent={() => console.log("DOWNLOAD")}/>
         </main>
     )
 };
