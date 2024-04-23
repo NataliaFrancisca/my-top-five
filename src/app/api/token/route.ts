ce53364bc5efe4e7cd2cb0ad8c27899abdc36e67
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-22 23:22:00 -0300

    feat: removed the secure cookie

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -4,8 +4,7 @@ import { getToken } from 'next-auth/jwt'
 export async function GET(req: NextRequest) {
     const token = await getToken({
         req,
-        secret: process.env.NEXTAUTH_SECRET ?? '',
-        secureCookie: process.env.NEXTAUTH_URL?.startsWith('https://') ?? process.env.VERCEL_ENV === 'previe'
+        secret: process.env.NEXTAUTH_SECRET ?? ''
     })
 
     if(!token){
