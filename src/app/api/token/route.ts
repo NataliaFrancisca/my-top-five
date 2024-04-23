ac273e34546ecd05b6e6965541a1d2a8ccd1c82d
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-22 22:20:13 -0300

    feat: created an api call to get the user token

---
+++ src/app/api/token/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+
+export async function GET(req: NextRequest) {
+    const token = await getToken({
+        req,
+        secret: process.env.NEXTAUTH_SECRET ?? '',
+    })
+
+    if(!token){
+        return NextResponse.json({
+            message: 'Unauthorized'
+        },{status: 401});
+    }
+
+    return NextResponse.json({accessToken: token.accessToken})
+}
