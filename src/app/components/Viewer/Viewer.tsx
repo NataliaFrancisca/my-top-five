e2aa2a724ad3d67317fc7fe109212d843a5fb117
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-22 22:22:38 -0300

    feat: called functions to get the user token and fetch the data from api

--- src/app/components/Viewer/Viewer.tsx
+++ src/app/components/Viewer/Viewer.tsx
@@ -1,13 +1,21 @@
 import Image from "next/image";
 import Songs from "../Songs/Songs";
+import { fetchUserToken } from "@/service/token";
+import { getTopTrack } from "@/service/top-track";
 
-const Viewer = () => {
+export default async function Viewer(){
+
+    const usertoken = await fetchUserToken();
+
+    const fetchAPI = getTopTrack(usertoken);
+    const userdata = await fetchAPI();
+
     return(
         <section className="bg-c_black flex flex-col items-center py-8 px-2 justify-center">
             
             <Image src='title-h.svg' width={319} height={10} alt="title writting MY TOP FIVE" className="sm:max-w-full" />
 
-            <Songs />
+            <Songs userData={userdata.items} />
 
             <span className="text-center text-sm text-c_white">discover your top five <br /> <b className="text-base">my-top-five.vercel.app</b></span>
             
@@ -16,5 +24,3 @@ const Viewer = () => {
         </section>
     )
 }
-
-export default Viewer;
