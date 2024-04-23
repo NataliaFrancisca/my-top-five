3440def742dab5600c67f11f3638afcfae329697
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-22 22:21:48 -0300

    feat: fill the component with the user data; created a statement to handle with the big artists array

--- src/app/components/Song/Song.tsx
+++ src/app/components/Song/Song.tsx
@@ -1,13 +1,22 @@
+import { IFetchSong } from "@/types/interface";
 import Image from "next/image"
 
-const Song = () => {
+const Song = (props: {song: IFetchSong}) => {
+
+    const { album, artists, name, external_urls } = props.song;
+
     return(
         <article className="flex w-full gap-2">
-            <Image src='/teste.png' width={92} height={86} alt="album cover" />
+            <Image src={album.images[0].url} width={92} height={86} alt="album cover" />
 
             <div className="flex flex-col justify-center text-c_white">
-                <a className="font-bold text-xl no-underline" href="/">Chove Chuva</a>
-                <p className="font-medium text-base">Big Bllakk & 4 more</p>
+                <a className="font-bold text-xl no-underline" href={external_urls.spotify} target="_blank">{name}</a>
+
+                { artists.length < 2 ? artists.map((artist, key) => (
+                        <p key={key} className="font-medium text-base">{artist.name}</p>
+                    )) : <p className="font-medium text-base">{artists[0].name} & {artists.length - 1} more</p>
+                }
+
             </div>
         </article>
     )
