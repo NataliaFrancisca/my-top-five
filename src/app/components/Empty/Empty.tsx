7cee669db4f75905a3ade9e4577f1e4526569ff6
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-23 22:24:16 -0300

    feat: added margin style

--- src/app/components/Empty/Empty.tsx
+++ src/app/components/Empty/Empty.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Empty = () => {
     return(
-        <section className="bg-c_black flex flex-col items-center py-14 px-6 justify-center gap-10">
+        <section className="bg-c_black flex flex-col items-center py-14 px-6 justify-center gap-10 h-dvh my-10 mx-6">
             <Image src='title-error.svg' width={319} height={10} alt="title writting MY TOP FIVE" className="sm:max-w-full" />
 
             <Image src='illustration-white-version.svg' width={249} height={246} alt="title writting MY TOP FIVE" className="sm:max-w-full" />
