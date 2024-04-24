f264f3d728c6549a99967760a8dd5130ef701e9c
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-23 22:24:04 -0300

    feat: deleted the viewer page and make the api calls inside the view page

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,18 +1,41 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import Viewer from "../components/Viewer/Viewer";
+
+import { fetchUserToken } from "@/service/token";
+import { getTopTrack } from "@/service/top-track";
+
+import Image from "next/image";
+import Empty from "../components/Empty/Empty";
+import Songs from "../components/Songs/Songs";
 import ButtonDownload from "../components/Button/ButtonDownload";
 
 export default async function Page(){
     const session = await getServerSession();
+    const token = await fetchUserToken();
 
-    if(!session){
+    if(!session || !token){
         redirect('/');
+    }
+
+    const onFetchUserData = getTopTrack(token);
+    const userdata = await onFetchUserData();
+    
+    if(userdata.items.length == 0){
+        return <Empty />
     }
 
     return(
         <main className="py-10 px-6 flex flex-col md:mx-auto md:w-6/12 lg:w-4/12">
-            <Viewer />
+            <section className="bg-c_black flex flex-col items-center py-8 px-2 justify-center">
+                <Image src='title-h.svg' width={319} height={10} alt="title writting MY TOP FIVE" className="sm:max-w-full" />
+
+                <Songs userData={userdata.items} />
+
+                <span className="text-center text-sm text-c_white">discover your top five <br /> <b className="text-base">my-top-five.vercel.app</b></span>
+
+                <Image src="/spotify-logo.png" width={70} height={20} alt="Spotify logo" className="mt-2"/>
+            </section>
+           
             <ButtonDownload />
         </main>
     )
