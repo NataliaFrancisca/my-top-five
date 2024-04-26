8c83c7397234784db7d73ab32553ee849f3fc20f
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-25 22:51:07 -0300

    feat: changed the image for a text and return the error component if something get wrong

--- src/app/components/Viewer/Viewer.tsx
+++ src/app/components/Viewer/Viewer.tsx
@@ -6,27 +6,34 @@ import Image from "next/image";
 import Songs from "../Songs/Songs";
 
 import Empty from "../Empty/Empty";
+import Error from "../Error/Error";
 import ButtonDownload from "../Button/ButtonDownload";
 
 export default function Viewer(props: {userData: Array<IFetchSong>}){
 
     const ref = useRef() as React.MutableRefObject<HTMLElement>;
 
-    if(props.userData.length === 0){
+    if(props.userData === undefined){
+        return <Error />
+    }
+
+    if(props.userData.length === 0 || !props.userData){
         return <Empty />
     }
 
     return(
-        <main className="py-10 px-6 flex flex-col md:mx-auto md:w-6/12 lg:w-4/12">
-            <section className="bg-c_black flex flex-col items-center py-8 px-2 justify-center" ref={ref}>
-                <Image src='title-h.svg' width={319} height={10} alt="title writting MY TOP FIVE" className="sm:max-w-full" />
+        <main className="py-10 px-6 flex flex-col h-auto md:mx-auto md:w-6/12 lg:w-4/12">
+
+            <section className="bg-c_black flex flex-col items-center py-8 px-2 justify-center h-auto" ref={ref}>
+
+                <h1 className="text-5xl font-black text-primary text-center md:text-5xl">MY TOP FIVE</h1>
 
                 <Songs userData={props.userData} />
 
                 <span className="text-center text-sm text-c_white">discover your top five <br /> <b className="text-base">my-top-five.vercel.app</b></span>
                 
                 <Image src="/spotify-logo.png" width={70} height={20} alt="Spotify logo" className="mt-2"/>
-             </section>
+            </section>
 
             <ButtonDownload refHtmlToImage={ref}/>
         </main>
