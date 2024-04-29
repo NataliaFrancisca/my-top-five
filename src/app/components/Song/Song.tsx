76a5c9b7749e0d466f8ffed5a9f5fa20ebf95459
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-29 14:57:45 -0300

    refactor: changed some styles to a better mobile version

--- src/app/components/Song/Song.tsx
+++ src/app/components/Song/Song.tsx
@@ -6,17 +6,16 @@ const Song = (props: {song: IFetchSong}) => {
     const { album, artists, name, external_urls } = props.song;
 
     return(
-        <article className="flex w-full gap-2">
+        <article className="flex w-full gap-2 ">
             <Image src={album.images[0].url} width={92} height={86} alt="album cover" />
 
             <div className="flex flex-col justify-center text-c_white overflow-hidden whitespace-nowrap">
                 <a className="font-bold text-xl no-underline inline-block truncate" href={external_urls.spotify} target="_blank">{name}</a>
 
                 { artists.length < 2 ? artists.map((artist, key) => (
-                        <p key={key} className="font-medium text-base">{artist.name}</p>
-                    )) : <p className="font-medium text-base">{artists[0].name} & {artists.length - 1} more</p>
+                        <p key={key} className="font-medium text-lg">{artist.name}</p>
+                    )) : <p className="font-medium text-lg">{artists[0].name} & {artists.length - 1} more</p>
                 }
-
             </div>
         </article>
     )
--- src/app/components/Songs/Songs.tsx
+++ src/app/components/Songs/Songs.tsx
@@ -4,7 +4,7 @@ import Song from "../Song/Song"
 const Songs = (props: {userData: Array<IFetchSong>}) => {
 
     return(
-        <section className="w-10/12 flex flex-col mt-12 mb-10 gap-3">
+        <section className="w-11/12 flex flex-col mt-12 mb-10 gap-3">
             {props.userData.map((song, key) => (
                 <Song song={song} key={key}/>
             ))}
