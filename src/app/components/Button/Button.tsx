eeab3a126ca35718ebffc5ae52ae28890a772201
nataliafrancisca <nathifrancisca@gmail.com>
2024-08-15 20:54:31 -0300

    feat: import ts interface and change hover style

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,12 +1,17 @@
-export const Button = (props: {message: string, style: string, buttonEvent: () => void}) => {
+import { IButton } from '@/types/interface';
 
-    const default_style = "mt-6 py-5 border-solid border-[3px] rounded-sm border-c_black font-black text-c_black transition ease-in-out delay-150 duration-300 hover:text-primary"
+export const Button = (props: IButton) => {
+  const default_style =
+    'mt-6 py-5 border-solid border-[3px] rounded-sm border-c_black font-black text-c_black transition ease-in-out delay-150 duration-300 hover:';
 
-    return (
-        <button className={`${default_style} ${props.style}`} onClick={() => props.buttonEvent()}>
-            {props.message}
-        </button>
-    )
+  return (
+    <button
+      className={`${default_style} ${props.style}`}
+      onClick={() => props.event()}
+    >
+      {props.message}
+    </button>
+  );
 };
 
 export default Button;
