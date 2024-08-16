023f9c9e2a7cdf00310c5ad3b7d64494d4ce89f7
nataliafrancisca <nathifrancisca@gmail.com>
2024-08-15 21:26:32 -0300

    feat: change width size

--- src/app/components/Songs/Songs.tsx
+++ src/app/components/Songs/Songs.tsx
@@ -1,15 +1,14 @@
-import { IFetchSong } from "@/types/interface";
-import Song from "../Song/Song"
+import { IFetchSong } from '@/types/interface';
+import Song from '../Song/Song';
 
-const Songs = (props: {userData: Array<IFetchSong>}) => {
-
-    return(
-        <section className="w-11/12 flex flex-col mt-12 mb-10 gap-3">
-            {props.userData.map((song, key) => (
-                <Song song={song} key={key}/>
-            ))}
-        </section>
-    )
-}
+const Songs = (props: { userData: Array<IFetchSong> }) => {
+  return (
+    <section className="w-full flex flex-col my-10 gap-3">
+      {props.userData.map((song, key) => (
+        <Song song={song} key={key} />
+      ))}
+    </section>
+  );
+};
 
 export default Songs;
