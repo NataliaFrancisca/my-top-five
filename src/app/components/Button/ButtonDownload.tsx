0b2f742835593e3c191c87eb41b5daec682a6d9e
nataliafrancisca <nathifrancisca@gmail.com>
2024-08-15 21:36:20 -0300

    fix: update button event function

--- src/app/components/Button/ButtonDownload.tsx
+++ src/app/components/Button/ButtonDownload.tsx
@@ -1,10 +1,16 @@
 'use client';
-import { RefObject } from "react";
-import Button from "./Button";
-import { htmlToImageConvert } from "@/utils/generateImage";
+import { RefObject } from 'react';
+import Button from './Button';
+import { htmlToImageConvert } from '@/utils/generateImage';
 
-const ButtonDownload = (props: {refHtmlToImage: RefObject<HTMLElement>}) => {
-    return <Button message="DOWNLOAD" style="w-8/12 self-end bg-c_black text-c_white text-base" buttonEvent={() => htmlToImageConvert(props.refHtmlToImage.current)}/>
-}
+const ButtonDownload = (props: { refHtmlToImage: RefObject<HTMLElement> }) => {
+  return (
+    <Button
+      message="DOWNLOAD"
+      style="w-8/12 self-end bg-c_black text-c_white text-base"
+      event={() => htmlToImageConvert(props.refHtmlToImage.current)}
+    />
+  );
+};
 
 export default ButtonDownload;
